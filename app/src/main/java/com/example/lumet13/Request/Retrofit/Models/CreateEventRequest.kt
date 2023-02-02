1eec5617316526e3e6b6672bf83c489676f6dac5
AlexanderTaran73 <alexhock@yandex.ru>
2023-02-02 16:46:21 +0300

    ServInterface_reg

---
+++ app/src/main/java/com/example/lumet13/Request/Retrofit/Models/CreateEventRequest.kt
@@ -0,0 +1,15 @@
+package com.example.lumet13.Request.Retrofit.Models
+
+class CreateEventRequest(
+    val name : String,
+    val description : String,
+    val hobbytype : String,
+    val time : String,
+    val desiredage : Int?,
+    val participantLimit : Int?,
+    val participantsAnonymity : String,
+    val privacyStatus : String,
+    val registrationSettings : String,
+    val latitude : String,
+    val longitude : String
+)
--- app/src/main/java/com/example/lumet13/Request/Retrofit/ServInterface_reg.kt
+++ app/src/main/java/com/example/lumet13/Request/Retrofit/ServInterface_reg.kt
@@ -1,10 +1,7 @@
 package com.example.lumet13.Request.Retrofit
 
 import android.media.Rating
-import com.example.lumet13.Request.Retrofit.Models.Login
-import com.example.lumet13.Request.Retrofit.Models.Token
-import com.example.lumet13.Request.Retrofit.Models.User
-import com.example.lumet13.Request.Retrofit.Models.UserDTO
+import com.example.lumet13.Request.Retrofit.Models.*
 import lumetbackend.entities.EventDTO
 import okhttp3.ResponseBody
 import retrofit2.Call
@@ -30,11 +27,11 @@ interface ServInterface_reg {
     fun Authorization(@Body login: Login?): Call<String>
 
     //@Headers("Content-Type: application/json")
+
+//    Users
     @GET("users/getUser")
     fun GetUser(): Call<UserDTO>
 
-    @GET("events/get_All_events")
-    fun GetEvents(): Call<List<EventDTO>>
 
     @GET("users/getALLUsers")
     fun GetALLUsers(): Call<List<UserDTO>>
@@ -49,6 +46,139 @@ interface ServInterface_reg {
         @Path("rating") rating: Int,
         @Path("hobby") hobby: String
     ): Call<List<UserDTO>>
+
+
+
+//change_user
+    @POST("users/change_user/change_login/{login}")
+    fun changeLogin(
+        @Path("login") login:String
+    ): Call<ResponseBody>
+
+    @POST("users/change_user/change_email/{email}")
+    fun changeEmail(
+        @Path("email") email:String
+    ): Call<ResponseBody>
+
+    @POST("users/change_user/change_email/confirmation/{email}/{token}")
+    fun changeEmailConfirmation(
+        @Path("email") email:String,
+        @Path("token") token:Int
+    ): Call<ResponseBody>
+
+    @POST("users/change_user/change_password/{password}")
+    fun changePassword(
+        @Path("password") password:String
+    ): Call<ResponseBody>
+
+    @POST("users/change_user/change_age/{age}}")
+    fun changeAge(
+        @Path("age") age:String
+    ): Call<ResponseBody>
+
+    @POST("users/change_user/change_privacystatus/profile/{status}")
+    fun changeProfilePrivacyStatus(
+        @Path("status") status:String
+    ): Call<ResponseBody>
+
+    @POST("users/change_user/change_privacystatus/map/{status}")
+    fun changeMapPrivacyStatus(
+        @Path("status") status:String
+    ): Call<ResponseBody>
+
+    @POST("users/change_user/change_privacystatus/chat/{status}")
+    fun changeChatPrivacyStatus(
+        @Path("status") status:String
+    ): Call<ResponseBody>
+
+
+    @POST("users/change_user/change_hobbytype/{hobbytypeName}")
+    fun changeHobbyType(
+        @Path("hobbytypeName") hobbytypeName:String
+    ): Call<ResponseBody>
+
+//Events
+    @GET("events/get_All_events")
+    fun GetEvents(): Call<List<EventDTO>>
+
+
+    @POST("events/create_event")
+    fun changeHobbyType(@Body createEventRequest: CreateEventRequest): Call<ResponseBody>
+
+    @POST("events/remove_event/{eventId}")
+    fun removeEvent(
+        @Path("eventId") eventId:Int
+    ): Call<ResponseBody>
+
+    @POST("events/change_evemt/{eventId}")
+    fun changeEvent(
+        @Body createEventRequest: CreateEventRequest,
+        @Path("eventId") eventId:Int
+    ): Call<ResponseBody>
+
+    @GET("events/get_All_events/sort_and_search/{search}/{min_age}/{rating}/{hobby}/{users_limit}")
+    fun getAllEventsSort(
+        @Path("search") search:String,
+        @Path("min_age") min_age:Int,
+        @Path("rating") rating: Int,
+        @Path("hobby") hobby: String,
+        @Path("users_limit") users_limit: String
+
+    ): Call<List<EventDTO>>
+
+
+
+//Interaction
+//    Events
+
+    @POST("users/users_interaction/apply_for_event/{eventId}")
+    fun changeEvent(
+        @Path("eventId") eventId:Int
+    ): Call<ResponseBody>
+
+    @POST("users/users_interaction/remove_apply_for_event/{eventId}")
+    fun removeApplyForEvent(
+        @Path("eventId") eventId:Int
+    ): Call<ResponseBody>
+
+    @POST("users/users_interaction/confirm_apply_for_event/{eventId}/{userId}")
+    fun confirmApplyForEvent(
+        @Path("eventId") eventId:Int,
+        @Path("userId") userId:Int
+    ): Call<ResponseBody>
+
+
+//      Friends
+
+    @POST("users/users_interaction/friend_request/{userId}")
+    fun friendRequest(
+        @Path("userId") userId:Int
+    ): Call<ResponseBody>
+
+    @POST("users/users_interaction/cancel_friend_request/{userId}")
+    fun cancelFriendRequest(
+        @Path("userId") userId:Int
+    ): Call<ResponseBody>
+
+    @POST("users/users_interaction/accept_request/{userId}")
+    fun acceptRequest(
+        @Path("userId") userId:Int
+    ): Call<ResponseBody>
+
+    @POST("users/users_interaction/reject_request/{userId}")
+    fun rejectRequest(
+        @Path("userId") userId:Int
+    ): Call<ResponseBody>
+
+    @POST("users/users_interaction/remove_from_friends/{userId}")
+    fun removeFromFriends(
+        @Path("userId") userId:Int
+    ): Call<ResponseBody>
+
+
+
+
+
 
     @GET("test")
     fun test(): Call<ResponseBody?>?
